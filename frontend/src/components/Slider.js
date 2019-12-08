39ca861cf7aa4a6ce4d325f1b6b6774879efa9ce
NicoRondan <nicolasrondan@live.com.ar>
2019-12-07 21:03:31 -0300

    mejorando el slider/carousel

--- frontend/src/components/Slider.js
+++ frontend/src/components/Slider.js
@@ -8,19 +8,30 @@ import image3 from './../assets/cg6.jpg';
 
 const Slider = () => {
     return (
-        <div id="carouselControls" class="carousel slide" data-ride="carousel" >
-      <div id="home" class="carousel-inner">
-        <div class="carousel-item active"><img class="d-block w-100" src={image1} alt="pantalon"/></div>
-        <div class="carousel-item item"><img class="d-block w-100" src={image2} alt="ropa"/></div>
-        <div class="carousel-item item"><img class="d-block w-100" src={image3} alt="calzado"/></div>
+      <div id="carouselControls" className="carousel slide home-carousel" data-ride="carousel" >
+      <div id="home" className="carousel-inner homepage owl-carousel">
+        <div className="dark-mask mask-primary"></div>
+        <div className="carousel-item active item">
+          <div className="row">
+              <img className="d-block w-100" src={image1} alt="pantalon"/>
+            </div>
+          </div>
+        <div className="carousel-item item">
+        <div className="row">
+          <img className="d-block w-100" src={image2} alt="ropa"/></div>
+        </div>
+        <div className="carousel-item item">
+        <div className="row">
+          <img className="d-block w-100" src={image3} alt="calzado"/></div>
       </div>
-      <a class="carousel-control-prev" href="#carouselControls" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Anterior</span>
+      </div>
+      <a className="carousel-control-prev" href="#carouselControls" role="button" data-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="sr-only">Anterior</span>
       </a>
-      <a class="carousel-control-next" href="#carouselControls" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Siguiente</span>
+      <a className="carousel-control-next" href="#carouselControls" role="button" data-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="sr-only">Siguiente</span>
       </a>
     </div>  
     );
