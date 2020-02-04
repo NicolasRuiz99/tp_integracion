39833b5ea36c87c709b89780aae5aa2b7ad7b13f
NicoRondan <nicolasrondan@live.com.ar>
2020-02-04 15:39:26 -0300

    agrego puntero al user en review

--- frontend/src/components/admin/details/ReviewDetail.js
+++ frontend/src/components/admin/details/ReviewDetail.js
@@ -4,7 +4,7 @@ import { getReview, deleteReview } from '../../pages/customer/utils/CustomerFunc
 import Spinner from 'react-bootstrap/Spinner';
 import Rating from '../../pages/shop/Rating';
 import Button from 'react-bootstrap/Button';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 import DeleteReviewModal from './../../modals/DeleteReviewModal';
 import Error from '../../messages/Error';
 
@@ -15,6 +15,7 @@ function ReviewDetail({props, history}) {
     const [error, setError] = useState(false);
     const [loading,setLoading] = useState (false);
     const [id] = useState(props.match.params.id);
+    const [link, setLink] = useState('');
 
     const handleModalOpen = () => {
         setModalOpen(!modalOpen);
@@ -25,8 +26,9 @@ function ReviewDetail({props, history}) {
         setLoading(true);
         getReview(id)
         .then(res => {
-            //console.log(res);
+            console.log(res);
             setReview(res);
+            setLink(`/admin-page/customer-detail/${res.id_user}`);
             setLoading(false);
         })
         .catch(err => {
@@ -57,11 +59,12 @@ function ReviewDetail({props, history}) {
             ): ( (error) ? (<Error texto="Ha ocurrido un error interno en el servidor" />) : (
                 <div className="card shadow" style={{left:'35%',position:'absolute',bottom:'40%', width:'30%'}}>
                     <div className="card-header">
-                        <h3 className="text-uppercase text-center">
-                            {<label>
+                        <h3 className="text-center">
+                            <label>
                                 <span><Rating change={false} stars={review.stars} /></span>
-                            </label>}
+                            </label>
                         </h3>
+                        <Link className="float-right" to={link}> Usuario #{review.id_user}</Link>
                     </div>
                     <div className="card-body">
                         <div className="col-md-12 text-center">
