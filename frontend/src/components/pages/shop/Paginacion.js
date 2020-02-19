e56e1bb5877c36d839dcc110b27960a61bb3b558
NicolasRuiz99 <nicoruiz72@gmail.com>
2020-02-19 15:42:46 -0300

    paginacion arreglada

--- frontend/src/components/pages/shop/Paginacion.js
+++ frontend/src/components/pages/shop/Paginacion.js
@@ -15,13 +15,13 @@ const Paginacion = ({listPerPage, totalList, paginate, currentPage}) => {
         <div className="pages" > 
             <nav aria-label="Page navigation example" className="d-flex justify-content-center">
                 <ul className="pagination">
-                    <li className="page-item"><div onClick={() => paginate(currentPage - 1)} className="page-link" style={{cursor:'pointer'}}> <i className="fa fa-angle-double-left"></i></div></li>
+                    <li className={`page-item ${(currentPage === pageNumbers[0])?'disabled':null}`}><div onClick={() => paginate(currentPage - 1)} className="page-link" style={{cursor:'pointer'}}> <i className="fa fa-angle-double-left"></i></div></li>
                     {
                     pageNumbers.map(number => (
                         <li key={number} className={`page-item ${(currentPage === number) ? 
                             'active' : null}`}><div onClick={() => paginate(number)} className="page-link" style={{cursor:'pointer'}}>{number}</div></li>
                     ))}
-                    <li className="page-item"><div onClick={() => paginate(currentPage + 1)} className="page-link" style={{cursor:'pointer'}}><i className="fa fa-angle-double-right"></i></div></li>
+                    <li className={`page-item ${(currentPage === pageNumbers[pageNumbers.length - 1])? 'disabled':null}`}><div onClick={() => paginate(currentPage + 1)} className="page-link" style={{cursor:'pointer'}}><i className="fa fa-angle-double-right"></i></div></li>
                 </ul>
             </nav>
         </div>
